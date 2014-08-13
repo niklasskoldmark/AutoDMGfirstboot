ab7130de8143f5fa7c615e6ece0111fe67b2d728
Niklas Sköldmark <niklas.skoldmark@gmail.com>
2014-08-13 09:21:36 +0200

    customized

--- ROOT/Library/Scripts/medborgarskolan/firstboot.sh
+++ ROOT/Library/Scripts/medborgarskolan/firstboot.sh
@@ -10,25 +10,47 @@ genericppd="/System/Library/Frameworks/ApplicationServices.framework/Versions/A/
  
 # Delete iMovie (Previous Version) Directory if it exists, because we don't need it. 
 #Use when installing iLife using the original installers with InstaDMG.
-rm -R /Applications/iMovie\ \(previous\ version\).localized/
+#rm -R /Applications/iMovie\ \(previous\ version\).localized/
  
 # ARD Configuration
-#Enable ARD for localadmin
+#Enable ARD for admin
 $kickstart -configure -allowAccessFor -specifiedUsers
-$kickstart -activate -configure -access -on -users "localadmin" -privs -all -restart -agent
+$kickstart -activate -configure -access -on -users "admin" -privs -all -restart -agent
  
 # Set time zone and time server.
 $systemsetup -setusingnetworktime on
-$systemsetup -settimezone America/New_York -setnetworktimeserver time.apple.com
+$systemsetup -settimezone Europe/Stockholm -setnetworktimeserver time.euro.apple.com
  
+#Removes Time Machine from the menu
+#defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup 1
+ 
+#disable ipv6
+#$networksetup -setv6off Ethernet
+
+
+# Activate WakeOnLAN.
+#$systemsetup -setwakeonnetworkaccess on 
+
 #Sets HostName
-scutil --set HostName $(scutil --get LocalHostName)
+#scutil --set HostName $(scutil --get LocalHostName)
+base=""
+tld="admbs.mbs08.se"
+serial=`/usr/sbin/system_profiler SPHardwareDataType | /usr/bin/awk '/Serial\ Number\ \(system\)/ {print $NF}'`
+scutil --set ComputerName $base$serial
+scutil --set LocalHostName $base$serial
+scutil --set HostName "${base}${serial}.${tld}"
+
+
+
+defaults write /Library/Preferences/.GlobalPreferences AppleLocale = sv_SE
+defaults write /Library/Preferences/.GlobalPreferences Country SE
+
  
 #Sets computer to never sleep while plugged in to a power source
-pmset -c sleep 0
+#pmset -c sleep 0
  
 #Adds Printers
-lpadmin -p "Printer Name" -L "Printer Location" -D "Printer Description" -E -v lpd://server/printqueue -P $genericppd -o printer-is-shared=false
+#lpadmin -p "Printer Name" -L "Printer Location" -D "Printer Description" -E -v lpd://server/printqueue -P $genericppd -o printer-is-shared=false
  
 #Securely removes the launchd item and script.
 srm /Library/LaunchDaemons/edu.medborgarskolan.firstboot.plist
